bdd50b6f452fb0f839df312235e45c73911da134
Daniel Alonso <dani.alonso.garcia@gmail.com>
2017-10-02 03:11:16 -0400

    Reorganized code structure, added wikipedia API call

--- js/script.js
+++ js/script.js
@@ -6,47 +6,61 @@ $(document).ready(function() {
         var $nytHeaderElem = $('#nytimes-header');
         var $nytElem = $('#nytimes-articles');
         var $greeting = $('#greeting');
-        
-        // clear out old data before new request
-        $wikiElem.text("");
-        $nytElem.text("");
-        
-        // load streetview
+        var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+        var wikipediaUrl = 'https://en.wikipedia.org/w/api.php';
         var streetStr = $('#street').val();
         var cityStr = $('#city').val();
         var bgImgSize = '1920x1024';
         var bgImgUrl = 'https://maps.googleapis.com/maps/api/streetview?size=' + bgImgSize + '&location=' + streetStr + ', ' + cityStr;
+
+        // clear out old data before new request
+        $wikiElem.text("");
+        $nytElem.text("");
         
-        // YOUR CODE GOES HERE!
+        // load streetview    
+        $body.css('background-image', 'url("' + bgImgUrl + '")');
+        
         $greeting.text('So, you want to live at ' + streetStr + ', ' + cityStr + '?');
 
-        //$body.append('<img class="bgimg" src="' + bgImgUrl + '">');
-        $body.css('background-image', 'url("' + bgImgUrl + '")');
-
-
-
-        var nytUrl = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-        nytUrl = nytUrl + '?' + $.param({
+        $.getJSON(nytUrl, 
+            {
             'api-key': '027ce0de69ca4bb38a3a6192d9ea78ac',
             'q': cityStr,
             'fl': 'web_url,snippet,headline,document_type',
             'sort': 'newest'
-        });
-
-        $.getJSON(nytUrl, function(data) {
-            $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-            var allArticles = data.response.docs;
-            for (var i = 0; i < allArticles.length; i++) {
-                documentType = allArticles[i].document_type
-                if (documentType === 'article') {                  
-                    $nytElem.append('<li class="list-group-item article"><a target="_blank" href="' + allArticles[i].web_url + '">' + allArticles[i].headline.main + '</a><p>' + allArticles[i].snippet + '</p></li>');
-                };
+            },            
+            function(data) {
+                $nytHeaderElem.text('New York Times Articles About ' + cityStr);
+                var allArticles = data.response.docs;
+                for (var i = 0; i < allArticles.length; i++) {
+                    documentType = allArticles[i].document_type
+                    if (documentType === 'article') {                  
+                        $nytElem.append('<li class="list-group-item article"><a target="_blank" href="' + allArticles[i].web_url + '">' + allArticles[i].headline.main + '</a><p>' + allArticles[i].snippet + '</p></li>');
+                    };
+                }
             }
-
-        }).fail(function() {
+        ).fail(function() {
             $nytHeaderElem.addClass('alert alert-danger');
             $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+        });
+
+        $.ajax({
+            method: 'GET',
+            url: wikipediaUrl,
+            dataType: 'jsonp',
+            data: {
+                format: 'json',
+                action: 'opensearch',
+                search: cityStr,
+                limit: 20,
+            },
+            success: function(data) {
+                var allWikipediaLinks = data[3];
+                var allWikipediaLinksTitles = data[1];
+                for (var i = 0; i < allWikipediaLinks.length; i++) {
+                    $wikiElem.append('<li class="list-group-item"><i class="fa fa-caret-right" aria-hidden="true"></i><a target="_blank" href="' + allWikipediaLinks[i] + '"> ' + allWikipediaLinksTitles[i] + '</a>');
+                }
+            }
         });
 
         // $.each(allArticles, function(index, web_url) {
